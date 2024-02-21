be4ce61c9d978c1b301b70b8b68472347dcf532e
Elm2006 <isel06002@utb.vaxjo.se>
2024-02-21 17:28:47 +0100

    Add project files.

---
+++ inlämning6.11/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Vad ska ena sidan av figuren va");
+        int storlek = int.Parse(Console.ReadLine());
+        Console.WriteLine("Vilket tecken ska de vara?");
+        char tecken = char.Parse(Console.ReadLine());
+        Console.WriteLine("Vad ska avståndet vara");
+        int avstånd = int.Parse(Console.ReadLine());
+        if( avstånd > 0)
+        {
+            RitaKvadrat(storlek, tecken, avstånd);
+        }
+        else
+        {
+
+            RitaKvadrat(storlek, tecken);
+        }
+    }
+    static void RitaKvadrat(int x, char c, int y)
+    {
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                Console.Write(c);
+                for (int k = 0; k < y; k++)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+            for(int k = 0; k < y; k++)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+    static void RitaKvadrat(int x, char c)
+    {
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+        }
+    }
+}
